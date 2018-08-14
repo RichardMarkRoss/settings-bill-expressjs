ae7bb1d3dee708f575fb49bbd17884fa042fdd52
RichardMarkRoss <cptrichkidprice@gmail.com>
2018-08-14 15:15:45 +0200

    commit travis 7

--- test/test.js
+++ test/test.js
@@ -25,3 +25,34 @@ describe('test the if the string values hold the amount', function () {
         assert.equal(12, settings.smsTotal());
     });
 });
+describe('test the store the warning and critical values', function () {
+    it('should test the critical levels', function () {
+        assert.equal(10, settings.critical(10));
+    });
+    it('should test the warning levels', function () {
+        assert.equal(10, settings.warning(10));
+    });
+});
+describe('should return the total on the amount of calls and sms', function () {
+    it('should calculate two call', function () {
+        settings.updateCall(3);
+        settings.updateSms(0);
+        settings.calculate('call');
+        settings.calculate('call');
+        assert.equal('10.00', settings.total());
+    });
+    it('should calculate two sms', function () {
+        settings.updateCall(0);
+        settings.updateSms(3);
+        settings.calculate('sms');
+        settings.calculate('sms');
+        assert.equal('10.00', settings.total());
+    });
+    it('should calculate one sms and one call', function () {
+        settings.updateCall(3);
+        settings.updateSms(3);
+        settings.calculate('call');
+        settings.calculate('sms');
+        assert.equal('10.00', settings.total());
+    });
+});
